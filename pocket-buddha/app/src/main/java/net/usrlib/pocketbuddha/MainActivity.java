fac3ada389b2e174d5b7882ebb7ec71d96531047
rgr-myrg <home@home.local>
2016-06-06 15:00:07 -0400

    Task 1: Project Set Up

--- pocket-buddha/app/src/main/java/net/usrlib/pocketbuddha/MainActivity.java
+++ pocket-buddha/app/src/main/java/net/usrlib/pocketbuddha/MainActivity.java
@@ -2,7 +2,7 @@ package net.usrlib.pocketbuddha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-	
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
